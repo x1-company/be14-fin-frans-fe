13e2b42595bdfe75ada7e99897a1b73613fa6c41
Sumin <emily9949@naver.com>
2025-06-22 16:26:12 +0900

    ✨ feat: 배포용 환경변수 설정 추가

--- src/components/auth/LoginForm.vue
+++ src/components/auth/LoginForm.vue
@@ -19,7 +19,7 @@ const togglePasswordVisibility = () => {
 const handleLogin = async () => {
 
   try {
-    const response = await fetch('http://localhost:8080/login', {
+    const response = await fetch('${BASE_URL}/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
